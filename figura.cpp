dd683b037da1f8bf5454bca875c0664d93dbecc5
jhosephu <jehuanacunep@unjbg.edu.pe>
2024-11-10 14:19:35 -0500

    actualizando codigos

--- figura.cpp
+++ figura.cpp
@@ -7,10 +7,15 @@ class figura{
 		string color;
 	public:
 		figura(string);
+		void mostrarDatos();
 };
 
 figura::figura(string c){
 	color=c;
+}
+
+void figura::mostrarDatos(){
+	cout<<"color: "<<color<<endl;
 }
 
 class circulo:public figura{
@@ -18,25 +23,185 @@ class circulo:public figura{
 		float radio;
 	public:
 		circulo(string, float);
-		void calcularArea(float);
-		void calcularPerimetro(float);
+		void mostrarCirculo();
+		float calcularArea();
+		float calcularPerimetro();
 };
 
 circulo::circulo(string c, float r):figura(c){
 	radio=r;
 }
 
-void circulo::calcularArea(float r){
-	float area;
-	area=3.14*pow(r,2)
+void circulo::mostrarCirculo(){
+	mostrarDatos();
+	cout<<"el circulo tiene un radio de "<<radio<<"m"<<endl;
 }
 
-void calcularPerimetro(float r){
+float circulo::calcularArea(){
+	float area;
+	area=3.14*pow(radio,2);
+
+	return area;
+}
+
+float circulo::calcularPerimetro(){
 	float perimetro;
+	perimetro=2*3.14*radio;
 	
+	return perimetro;
+}
+
+class rectangulo:public figura{
+	private:
+		float base;
+		float altura;
+	public:
+		rectangulo(string, float, float);
+		void mostrarRectangulo();
+		float calcularArea();
+};
+
+rectangulo::rectangulo(string c, float b, float h):figura(c){
+	base=b;
+	altura=h;
+}
+
+void rectangulo::mostrarRectangulo(){
+	mostrarDatos();
+	cout<<"base del rectangulo: "<<base<<"m"<<endl;
+	cout<<"altura del rectangulo: "<<altura<<"m"<<endl;
+}
+
+float rectangulo::calcularArea(){
+	float area;
+	area=base*altura;
+	return area;
+}
+
+class triangulo:public figura{
+	private:
+		float base;
+		float altura;
+		string tipo;
+	public:
+		triangulo(string, float, float, string);
+		void mostrarTriangulo();
+		float calcularArea();
+};
+
+triangulo::triangulo(string c, float b, float h, string t):figura(c){
+	base=b;
+	altura=h;
+	tipo=t;
+}
+
+void triangulo::mostrarTriangulo(){
+	mostrarDatos();
+	cout<<"base del triangulo: "<<base<<"m"<<endl;
+	cout<<"altura del triangulo: "<<altura<<"m"<<endl;
+	cout<<"tipo del triangulo: "<<tipo<<endl;
+}
+
+float triangulo::calcularArea(){
+	float area;
+	area=(base*altura)/2.;
+	return area;
 }
 
 int main(){
-	
+	string color;
+	float radio, base, altura;
+	string tipo;
+	float area, perimetro;
+	int n, m;
+	do{
+		cout<<"escoja entre los siguientes: "<<endl;
+		cout<<"1. figura (general) "<<endl;
+		cout<<"2. circulo "<<endl;
+		cout<<"3. rectangulo "<<endl;
+		cout<<"4. triangulo "<<endl;
+		cout<<"0. salir: ";
+		cin>>n;
+		switch(n){
+			case 1:
+				cout<<"ingrese el color de la figura: "; cin>>color;
+				do{
+				figura f(color);
+				f.mostrarDatos();
+				}while (0);
+				break;
+			case 2:
+				cout<<"ingrese el color del circulo: "; cin>>color;
+				cout<<"ingrese el radio del circulo: "; cin>>radio;
+				do{
+					circulo c(color, radio);
+					cout<<"que desea hacer?: "<<endl;
+					cout<<"1. mostrar datos "<<endl;
+					cout<<"2. calcular area "<<endl;
+					cout<<"3. calcular perimetro "<<endl;
+					cout<<"0. retroceder "<<endl;
+					cin>>m;
+					switch (m){
+						case 1:
+							c.mostrarCirculo();
+							break;
+						case 2:
+							area=c.calcularArea();
+							cout<<"el area del circulo es: "<<area<<endl;
+							break;
+						case 3:
+							perimetro=c.calcularPerimetro();
+							cout<<"el perimetro del circulo es: "<<perimetro<<endl;
+							break;
+					}
+				} while(m!=0);
+				break;
+			case 3:
+				cout<<"ingrese el color del rectangulo: "; cin>>color;
+				cout<<"ingrese la base del rectangulo: "; cin>>base;
+				cout<<"ingrese la altura del rectangulo: "; cin>>altura;
+				do{
+					rectangulo r(color, base, altura);
+					cout<<"que desea hacer?: "<<endl;
+					cout<<"1. mostrar datos "<<endl;
+					cout<<"2. calcular area "<<endl;
+					cout<<"0. retroceder "<<endl;
+					cin>>m;
+					switch (m){
+						case 1:
+							r.mostrarRectangulo();
+							break;
+						case 2:
+							area=r.calcularArea();
+							cout<<"el area del rectangulo es: "<<area<<endl;
+							break;
+					}
+				}while (m!=0);
+				break;
+			case 4:
+				cout<<"ingrese el color del triangulo: "; cin>>color;
+				cout<<"ingrese la base del triangulo: "; cin>>base;
+				cout<<"ingrese la altura del triangulo: "; cin>>altura;
+				cout<<"ingrese el tipo de triangulo: "; cin>>tipo;
+				do{
+					triangulo t(color, base, altura, tipo);
+					cout<<"que desea hacer?: "<<endl;
+					cout<<"1. mostrar datos "<<endl;
+					cout<<"2. calcular area "<<endl;
+					cout<<"0. retroceder "<<endl;
+					cin>>m;
+					switch (m){
+						case 1:
+							t.mostrarTriangulo();
+							break;
+						case 2:
+							area=t.calcularArea();
+							cout<<"el area del triangulo es: "<<area<<endl;
+							break;
+					}
+				}while (m!=0);
+				break;
+		}
+	}while (n!=0);
 	return 0;
 }
--- vehiculo.cpp
+++ vehiculo.cpp
@@ -23,13 +23,13 @@ vehiculo::vehiculo(string n, string c, string m, float v){
 
 void vehiculo::mostrarDatos(){
 	cout<<"placa "<<numeroPlaca<<", es de color "<<color<<", de marca "<<marca;
-	cout<<" y una velocidad de "<<velocimetro<<" m/s";
+	cout<<" y tiene una velocidad de "<<velocimetro<<" m/s";
 	cout<<endl;
 }
 
 void vehiculo::cambiarVelocidad(float v2){
 	velocimetro=v2;
-	cout<<"la velocidad fue cambiada a "<<velocimetro<<"m/s";
+	cout<<"la velocidad fue cambiada a "<<velocimetro<<"m/s"<<endl;
 }
 
 class automovil:public vehiculo{
@@ -39,8 +39,8 @@ class automovil:public vehiculo{
 	public:
 		automovil(string, string, string, float, int, int);
 		void mostrarAutomovil();
-		void subirPasajeros(int);
-		void bajarPasajeros(int);
+		int subirPasajeros(int);
+		int bajarPasajeros(int);
 };
 
 automovil::automovil(string n, string c, string m, float v, int prts, int psj):vehiculo(n, c, m, v){
@@ -53,14 +53,16 @@ void automovil::mostrarAutomovil(){
 	cout<<"es un automovil que tiene "<<cantidadPuertas<<" puertas y "<<cantidadPasajeros<<" pasajeros"<<endl;
 }
 
-void automovil::subirPasajeros(int psj){
+int automovil::subirPasajeros(int psj){
 	cantidadPasajeros=cantidadPasajeros+psj;
-	cout<<"los pasajeros actuales son: "<<cantidadPasajeros;
+	cout<<"los pasajeros actuales son: "<<cantidadPasajeros<<endl;
+	return cantidadPasajeros;
 }
 
-void automovil::bajarPasajeros(int psj){
+int automovil::bajarPasajeros(int psj){
 	cantidadPasajeros=cantidadPasajeros-psj;
-	cout<<"los pasajeros actuales son: "<<cantidadPasajeros;
+	cout<<"los pasajeros actuales son: "<<cantidadPasajeros<<endl;
+	return cantidadPasajeros;
 }
 
 class camion:public vehiculo{
@@ -83,16 +85,116 @@ void camion::mostrarCamion(){
 
 void camion::cambiarCarga(double crg){
 	carga=crg;
+	cout<<"la carga cambio a: "<<carga<<"kg"<<endl;
 }
 
 int main(){
-	vehiculo v("XXXXXXX", "azul", "toyota", 30);
-	v.mostrarDatos();
-	cout<<"automovil: "<<endl;
-	automovil a("XXXXXXX", "rojo", "ferrari", 50, 4, 15);
-	a.mostrarAutomovil();
-	cout<<"camion: "<<endl;
-	camion c("XXXXXXX", "verde", "RONCO", 40, 4.5);
-	c.mostrarCamion();
+	string placa, color, marca;
+	float velocidad;
+	int puertas, pasajeros;
+	double carga;
+	int n, m;
+	do{
+		cout<<"ingrese el tipo de vehiculo: "<<endl;
+		cout<<"1. vehiculo (general): "<<endl;
+		cout<<"2. automovil: "<<endl;
+		cout<<"3. camion: "<<endl;
+		cout<<"0. salir: "<<endl;
+		cin>>n;
+			switch (n){
+				case 1:
+					cout<<"ingrese la placa del vehiculo: "; cin>>placa;
+					cout<<"ingrese el color del vehiculo: "; cin>>color;
+					cout<<"ingrese la marca del vehiculo: "; cin>>marca;
+					cout<<"ingrese la velocidad del vehiculo: "; cin>>velocidad;
+					do{
+						vehiculo v(placa,color,marca,velocidad);
+						cout<<"que desea hacer?: "<<endl;
+						cout<<"1. mostrar datos: "<<endl;
+						cout<<"2. cambiar velocidad: "<<endl;
+						cout<<"0. retroceder: "<<endl;
+						cin>>m;
+						switch(m){
+							case 1:
+								v.mostrarDatos();
+								break;
+							case 2:
+								cout<<"ingrese la nueva velocidad: ";
+								cin>>velocidad;
+								v.cambiarVelocidad(velocidad);
+								break;
+						}
+					} while(m!=0);
+					break;
+				case 2:
+					cout<<"ingrese la placa del automovil: "; cin>>placa;
+					cout<<"ingrese el color del automovil: "; cin>>color;
+					cout<<"ingrese la marca del automovil: "; cin>>marca;
+					cout<<"ingrese la velocidad del autmovil: "; cin>>velocidad;
+					cout<<"ingrese la cantidad de puertas: "; cin>>puertas;
+					cout<<"ingrese la cantidad de pasajeros: "; cin>>pasajeros;
+					do{
+						automovil a(placa, color, marca, velocidad, puertas, pasajeros);
+						cout<<"que desea hacer?: "<<endl;
+						cout<<"1. mostrar datos: "<<endl;
+						cout<<"2. cambiar velocidad: "<<endl;
+						cout<<"3. subir pasajeros: "<<endl;
+						cout<<"4. bajar pasajeros: "<<endl;
+						cout<<"0. retroceder: "<<endl;
+						cin>>m;
+						switch(m){
+							case 1:
+								a.mostrarAutomovil();
+								break;
+							case 2:
+								cout<<"ingrese la nueva velocidad: ";
+								cin>>velocidad;
+								a.cambiarVelocidad(velocidad);
+								break;
+							case 3:
+								cout<<"ingrese la cantidad de pasajeros que suben: ";
+								cin>>pasajeros;
+								pasajeros=a.subirPasajeros(pasajeros);
+								break;
+							case 4:
+								cout<<"ingrese la cantidad de pasajeros que bajan: ";
+								cin>>pasajeros;
+								pasajeros=a.bajarPasajeros(pasajeros);
+								break;
+						}
+					} while(m!=0);
+					break;
+				case 3:
+					cout<<"ingrese la placa del camion: "; cin>>placa;
+					cout<<"ingrese el color del camion: "; cin>>color;
+					cout<<"ingrese la marca del camion: "; cin>>marca;
+					cout<<"ingrese la velocidad del camion: "; cin>>velocidad;
+					cout<<"ingrese la carga del camion: "; cin>>carga;
+					do{
+						camion c(placa, color, marca, velocidad, carga);
+						cout<<"que desea hacer?: "<<endl;
+						cout<<"1. mostrar datos: "<<endl;
+						cout<<"2. cambiar velocidad: "<<endl;
+						cout<<"3. cambiar carga: "<<endl;
+						cout<<"0. retroceder: "<<endl;
+						cin>>m;
+						switch(m){
+							case 1:
+								c.mostrarCamion();
+								break;
+							case 2:
+								cout<<"ingrese la nueva velocidad: ";
+								cin>>velocidad;
+								c.cambiarVelocidad(velocidad);
+								break;
+							case 3:
+								cout<<"ingrese la nueva carga: ";
+								cin>>carga;
+								c.cambiarCarga(carga);
+						}
+					} while(m!=0);
+					break;
+			}
+	}while (n!=0);
 	return 0;
 }
